0de98feba98df9030afae3714d56716fe7261a94
Tân DV <mchemistry95@gmail.com>
2023-02-21 17:43:57 +0700

    chore: update demo link in Readme

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -1,7 +1,7 @@
-const Location = ({title, position}) => {
+const Location = ({title, position, color="black"}) => {
     return (
         <>
-          <circle cx="60" cy="100" r="5" fill="black" />
+          <circle cx={position.x} cy={position.y} r="5" fill={color} />
           <circle cx="100" cy="60" r="5" fill="black" />
           <circle cx="140" cy="100" r="5" fill="black" />
           <circle cx="100" cy="140" r="5" fill="black" />
